eb1d20eba02c016dd581850c23bc03c6468dcd2a
Mehar Middha <meharmiddha@Mehars-MacBook-Pro.local>
2024-09-04 16:21:00 +0530

    i have changes the dimenions and also i have added the button and the select companion list

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,12 +1,13 @@
 import { Input } from "@/components/ui/input";
-import { SelectBudgetOptions } from "@/constants/options";
+import { SelectBudgetOptions, SelectCompanionList } from "@/constants/options";
 import React, { useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { Button } from "@/components/ui/button";
 
 function CreateTrip() {
   const [place, setPlace] = useState();
   return (
-    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-50 px-5 mt-10">
       <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
       <p className="mt-3 text-gray-500 text-xl">
         Just provide some basic information, and ourn trip planner will generate
@@ -36,18 +37,39 @@ function CreateTrip() {
         </div>
       </div>
       <div>
-        <h2 className="text-xl my-3 font-medium">
-          What is Your Budget?
-        </h2>
+        <h2 className="text-xl my-3 font-medium">What is Your Budget?</h2>
         <div className="grid grid-cols-3 gap-5 mt-5">
-          {SelectBudgetOptions.map((item,index)=>(
-            <div key={index} className="p-4 border cursor-pointer rounded-lg hover:shadow-lg">
+          {SelectBudgetOptions.map((item, index) => (
+            <div
+              key={index}
+              className="p-4 border cursor-pointer rounded-lg hover:shadow-lg"
+            >
               <h2 className="text-4xl">{item?.icon}</h2>
               <h2 className="font-bold text-lg">{item?.title}</h2>
               <h2 className="text-sm text-gray-500">{item?.desc}</h2>
             </div>
           ))}
         </div>
+      </div>
+      <div>
+        <h2 className="text-xl my-3 font-medium">
+          Who do you plan on traveling with on your next adventure?
+        </h2>
+        <div className="grid grid-cols-3 gap-5 mt-5">
+          {SelectCompanionList.map((item, index) => (
+            <div
+              key={index}
+              className="p-4 border cursor-pointer rounded-lg hover:shadow-lg"
+            >
+              <h2 className="text-4xl">{item?.icon}</h2>
+              <h2 className="font-bold text-lg">{item?.title}</h2>
+              <h2 className="text-sm text-gray-500">{item?.desc}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="my-10 justify-end flex">
+        <Button>Generate Trip</Button>
       </div>
     </div>
   );
