cb15bd950c9089a9dd457698c09b524b9c769f2c
Mehar Middha <meharmiddha@Mehars-MacBook-Pro.local>
2024-09-12 14:56:45 +0530

    i fetched the data from the firebase

--- src/view-trip/[tripId]/index.jsx
+++ src/view-trip/[tripId]/index.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { db } from '@/service/firebaseConfig';
+import { doc, getDoc } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { toast } from 'sonner';
 
 function ViewTrip() {
+    const { tripId } = useParams();
+    const [trip, setTrip] = useState([]);
+    useEffect(()=>{
+        tripId&&GetTripData();
+    },[tripId])
+    const GetTripData=async()=>{
+        const docRef=doc(db, 'AITrips', tripId);
+        const docSnap=await getDoc(docRef);
+        if(docSnap.exists()){
+            console.log("Document: ", docSnap.data());
+            setTrip(docSnap.data());
+        }
+        else{
+            console.log("No Such Document");
+            toast("No Trip Found 😞")
+        }
+    }
   return (
-    <div>ViewTrip</div>
+    <div>
+        {/* Information Section  */}
+
+        {/* Recommended Hotels  */}
+
+        {/* Daily Plan  */}
+
+        {/* Footer  */}
+    </div>
   )
 }
 
