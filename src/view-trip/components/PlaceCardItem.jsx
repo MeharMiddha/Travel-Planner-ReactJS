a36075695434cb142723566fb0f90d0d9f20aaa7
Mehar Middha <meharmiddha@Mehars-MacBook-Pro.local>
2024-09-16 16:05:29 +0530

    i have made the itineraries section of my website and also made the footer in which i have added the nearby places to visit their details and duration of the trip

--- src/view-trip/components/PlaceCardItem.jsx
+++ src/view-trip/components/PlaceCardItem.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { Button } from "@/components/ui/button";
 
 const PlaceCardItem = ({ place }) => {
   return (
-    <div>PlaceCardItem</div>
-  )
-}
+    <Link to={'https://www.google.com/maps/search/?api=1&query='+place?.name} target='_blank'>
+    <div className="border rounded-xl p-3 mt-2 flex flex-wrap gap-5 hover:scale-105 transition-all hover:shadow-md cursor-pointer xl:flex-nowrap">
+      <img
+        src="/place.jpg"
+        alt=""
+        className="xl:w-[130px] xl:h-[130px] rounded-xl "
+      />
+      <div>
+        <h2 className="font-bold text-lg text-black">{place.name}</h2>
+        <p className="text-sm text-gray-400">{place?.details}</p>
+        <div className="flex justify-between items-center">
+            <div className="flex flex-col">
+                <h2 className="mt-2 text-black">⏱️ {place?.time_to_travel}</h2>
+                <p className="text-sm text-blue-400">💵 Entry Price: {place?.ticket_pricing}</p>
+            </div>
+            <Button size="sm"><FaMapLocationDot /></Button>
+        </div>
+      </div>
+    </div>
+    </Link>
+  );
+};
 
-export default PlaceCardItem
+export default PlaceCardItem;
--- src/view-trip/components/PlacesToVisit.jsx
+++ src/view-trip/components/PlacesToVisit.jsx
@@ -7,14 +7,16 @@ const PlacesToVisit = ({ trip }) => {
         <h2 className='font-bold text-xl mt-5'>Places to Visit</h2>
         <div>
             {trip?.tripData?.itinerary.map((item,index)=>(
-                <div>
+                <div className='mt-5'>
                     <h2 className='font-medium text-lg'>Day {item?.day}</h2>
+                    <div className='grid md:grid-cols-2 gap-5'>
                     {item?.places.map((place,index)=>(
                         <div>
                             <h2 className='font-medium text-sm text-orange-600'>{place?.time_schedule}</h2>
                             <PlaceCardItem place={place} />
                         </div>
                     ))}
+                    </div>
                 </div>
             ))}
         </div>
